ffe8b17ab2ae4d07f22e0867b811c220cea13d52
laurens <lauren.smi001@gmail.com>
2023-05-19 18:06:36 -0400

    README updates, added screenshots

---
+++ README.md
@@ -0,0 +1,65 @@
+![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+# Note Taker App
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Technologies](#technologies)
+- [License](#license)
+
+## Description
+
+This Note Taker app can be used to create, view, edit, save and delete notes. The application's front end was already provided, and the challenge was to create an Express.js back end that will save and retrieve user-generated entries from a JSON file. 
+
+GitHub repository: https://github.com/LauSmi/note-taker-app
+
+Heroku deployment: https://note-taker-09.herokuapp.com/notes
+
+Link to video walk-through: 
+
+Screenshots:
+![Alt text](public/assets/images/note-taker1.png)
+![Alt text](public/assets/images/note-taker2.png)
+![Alt text](public/assets/images/note-taker3.png)
+
+## Installation
+
+To install necessary dependencies, run the following command in the terminal: npm i express
+
+
+## Usage
+
+Once all the packages have been installed, open terminal and run the following:
+
+node server.js
+
+This will run the server where you can find the page on localhost:3001.
+
+
+## Credits
+
+University of Minnesota lectures and materials.
+
+Starter code: https://github.com/coding-boot-camp/miniature-eureka
+
+For references and tutorials:
+
+https://w3schools.com
+https://developer.mozilla.org/en-US
+https://github.com/
+
+## Technologies
+
+Express.js
+Node.js
+HTML
+CSS
+JavaScript
+
+## License
+
+Licensed under the MIT license.
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 // GET requests
 app.get('/api/notes', (req, res) => {
-  res.json(entries.slice(3)); // Slice from index 2 to remove the first two notes
+  res.json(entries.slice(3)); 
 });
 
 app.get('/', (req, res) => {
@@ -81,91 +81,3 @@ app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}!`);
 });
 
-/* Sets port for listening
-const PORT = process.env.PORT || 3001;
-
-//Dependencies
-const fs = require('fs');
-const path = require('path');
-
-const express = require('express');
-const app = express();
-
-const entries = require('./db/db.json');
-
-//Middleware 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
-
-//GET requests
-app.get('/api/notes', (req, res) => {
-    res.json(entries.slice(1));
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-//Function to make new notes
-function makeNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
-
-//POST request
-app.post('/api/notes', (req, res) => {
-    const newNote = makeNewNote(req.body, entries);
-    res.json(newNote);
-});
-
-//Delete function
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
-
-            break;
-        }
-    }
-}
-
-//DELETE request
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, entries);
-    res.json(true);
-});
-
-
-//Port listener
-app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}!`);
-});*/
